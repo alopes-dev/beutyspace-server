4f8a1937809af84dfbe3fcc05dca22c77655d28b
alopes-dev <pdkdidy2011@gmail.com>
2021-12-07 17:24:37 +0100

    Feat: add user services

---
+++ src/app/services/UserServices.ts
@@ -0,0 +1,55 @@
+import { PrismaClient } from ".prisma/client";
+import { UserType } from "src/@types";
+import * as Yup from "yup";
+const prisma = new PrismaClient();
+
+class UserServices {
+  async getUsers() {
+    try {
+      const allUsers = await prisma.user.findMany();
+      return allUsers;
+    } catch (error) {
+      throw error;
+    } finally {
+      await prisma.$disconnect();
+    }
+  }
+
+  async createUser(user: UserType) {
+    try {
+      const schema = Yup.object().shape({
+        email: Yup.string().email().required(),
+        password: Yup.string().required().min(6),
+        role: Yup.string().required(),
+      });
+      const { email, password, role } = user;
+
+      if (!(await schema.isValid(user))) {
+        throw new Error("Validation fails");
+      }
+
+      const userExists = await prisma.user.findFirst({
+        where: { email },
+      });
+
+      if (userExists) {
+        throw new Error("User already exists.");
+      }
+
+      return await prisma.user.create({
+        data: { email, password, role, } as any,
+      });
+    } catch (error) {
+      throw error;
+    } finally {
+      await prisma.$disconnect();
+    }
+  }
+
+  async putUser(user: UserType){
+
+   
+  }
+}
+
+export default new UserServices();
